67b2ad10b11befcc0283552d62efd83b3a36a925
Shreyeska <silwalshreyeska@gmail.com>
2024-04-10 12:20:17 +0545

    Working on the Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
+
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
